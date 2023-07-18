4544afb0215ad6c89b42f8224c5a53237779e19f
Heitor Prado <heitorrdpp@gmail.com>
2023-07-18 18:31:53 -0300

    feat: add drag piece to create position

---
+++ src/components/Piece.jsx
@@ -0,0 +1,16 @@
+import {useDraggable} from "@dnd-kit/core"
+import {CSS} from "@dnd-kit/utilities"
+
+export default function Piece(props) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: "draggable-" + props.id,
+    data: { 
+      index: props.id,
+      type: props.type
+    }
+  });
+
+  return (
+    <img {...attributes} {...listeners} ref={setNodeRef} style={{transform: CSS.Translate.toString(transform)}} src={props.type + ".png"} alt=""/>
+  )
+}
--- src/pages/GameAnalysis/GameAnalysis.jsx
+++ src/pages/GameAnalysis/GameAnalysis.jsx
@@ -1,7 +1,44 @@
 import ChessBoard from "../../components/ChessBoard"
+import Piece from "../../components/Piece";
+import { DndContext, MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { useState } from "react"
 
 export default function GameAnalysis() {
+  const [squareChildren, setSquareChildren] = useState(null);
+
+  const mouseSensor = useSensor(MouseSensor); // Initialize mouse sensor
+  const touchSensor = useSensor(TouchSensor); // Initialize touch sensor
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const handleDragEnd = (e) => {
+    const parent = e.over ? e.over.id : null;
+    const sq = parseInt(parent.replace("droppable-", ""))
+
+    let row = Math.floor(sq / 8);
+    let col = sq % 8;
+
+    let sqChildrenCopy = JSON.parse(JSON.stringify(squareChildren));
+
+    sqChildrenCopy[row][col] = e.active.data.current?.type;
+    setSquareChildren(sqChildrenCopy);
+  }
+
   return (
-    <ChessBoard height='40px' width='40px'/>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+      <ChessBoard height='60px' width='60px' squareChildren={squareChildren} setSquareChildren={setSquareChildren}/>
+
+      <Piece type="p" id={1}/>
+      <Piece type="n" id={2}/>
+      <Piece type="k" id={3}/>
+      <Piece type="q" id={4}/>
+      <Piece type="b" id={5}/>
+
+      <Piece type="P" id={6}/>
+      <Piece type="N" id={7}/>
+      <Piece type="K" id={8}/>
+      <Piece type="Q" id={9}/>
+      <Piece type="B" id={10}/>
+
+    </DndContext>
   )
 }

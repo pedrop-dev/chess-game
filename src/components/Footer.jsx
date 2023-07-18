390257a6e7c026c0b553fc68c6e9dab4355291ba
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-07-18 18:44:03 -0300

    feat(style): footer design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../style/Footer.scss'
+import '../style/FooterResponsivity.scss'
+
+//Icons
+import { AiFillGithub } from "react-icons/ai"
+import { BsLinkedin } from "react-icons/bs"
+import { BiLogoDiscord } from "react-icons/bi"
+import { FaChessPawn } from "react-icons/fa"
+
+
+export default function Footer() {
+    return (
+        <footer className='footer_component'>
+            <section className="footer_section">
+                <div className="section_logo_wrapper">
+                    <FaChessPawn className='pawn_icon'/>
+                    <h2>Chess</h2>
+                </div>
+            </section>
+
+            <section className="footer_section">
+                <h2>Contact</h2>
+                <ul>
+                    <li><a href="mailto:contact@gmail.com">contact@gmail.com</a></li>
+                    <li><a href="mailto:contact@gmail.com">contact@gmail.com</a></li>
+                    <li><a href="mailto:contact@gmail.com">contact@gmail.com</a></li>
+                </ul>
+            </section>
+
+            <section className="footer_section">
+                <h2>About Us</h2>
+                <ul>
+                    <li>Help</li>
+                    <li>Features</li>
+                    <li>Privacy</li>
+                </ul>
+            </section>
+
+            <section className="footer_section">
+                <h2>Social</h2>
+                <div className="section_icons_wrapper">
+                    <a href="#">
+                        <AiFillGithub className='github_icon footer_icons'/>
+                    </a>
+                    <a href="#">
+                        <BsLinkedin className='linkedin_icon footer_icons'/>
+                    </a>
+                    <a href="#">
+                        <BiLogoDiscord className='discord_icon footer_icons'/>
+                    </a>
+                </div>
+            </section>
+        </footer>
+    )
+}

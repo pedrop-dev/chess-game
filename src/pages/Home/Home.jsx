5687b5ea41926819d7d7cd38c0ca777ef2c32388
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-07-20 17:08:10 -0300

    feat(style): sign in page style and composition

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import Footer from '../../components/Footer'
 import Nav from '../../components/Nav'
+import './Home.scss'
+
+//Icons 
+import { FaChessPawn } from "react-icons/fa"
+import { FaRobot } from "react-icons/fa"
 
 export default function Home() {
     return (
         <>
             <Nav />
+            <main className='home_main'>
+                <h1 className='home_main_h1'>Play Chess!</h1>
+                <section className='home_main_section'>
+                    <button className='home_section_online home_buttons'>
+                        <FaChessPawn/>Play Online
+                    </button>
+
+                    <button className='home_section_computer home_buttons'>
+                        <FaRobot/>Play Computer
+                    </button>
+                </section>
+                <section>
+                </section>
+
+            </main>
             <Footer />
         </>
     )
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,3 +1,41 @@
+import HeaderAccount from '../../components/HeaderAccount'
+import Footer from '../../components/Footer'
+
 export default function SignIn() {
-    
+    return (
+        <>
+            <HeaderAccount/>
+            <main className='main_signin'>
+                <section className="signin_section">
+                    <h1 className='section_signin_h1'>
+                        Enter your email and <br/>password
+                    </h1>
+                    <p className='section_signin_p'>
+                        Sign in allows you to <br/> access your account
+                    </p>
+                </section>
+
+                <form className="main_form_signin">
+                    <input 
+                        type="text" 
+                        placeholder='Email'
+                        className='form_signin_text'
+                    />
+
+                    <input 
+                        type="text" 
+                        placeholder='Password'
+                        className='form_signin_text'
+                    />
+
+                    <input 
+                        type="submit" 
+                        value="Sign in"
+                        className='form_signin_submit'
+                    />
+                </form>
+            </main>
+            <Footer/>
+        </>
+    )
 }

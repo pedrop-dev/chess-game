0c79bd14800520c778dec040add72d9c4efca595
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-08-03 21:22:01 -0300

    feat: style correction

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import './HomeResponsivity.scss'
 import io from 'socket.io-client'
 import { API_BASE_URL } from '../../constants.js'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 //Icons 
 import { FaChessPawn } from "react-icons/fa"
@@ -16,6 +17,7 @@ const socket = io(API_BASE_URL)
 export default function Home() {
     const [username, setUsername] = useState('')
 
+    
     const handleJoinRoom = () => {
       socket.emit('join_room', { username })
       console.log("JOINING ROOM")
@@ -33,18 +35,20 @@ export default function Home() {
             <Nav />
             <main className='home_main'>
                 <h1 className='home_main_h1'>Play Chess!</h1>
-                <section className='home_main_section'>
-
+                <section className='home_main_section'>  
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
 
-                    <button className='home_section_online home_buttons' onClick={handleJoinRoom}>
-                        <FaChessPawn/>Play Online
-                    </button>
+                    <Link to='/play-game'>
+                        <button className='home_section_online home_buttons' onClick={handleJoinRoom}>
+                            <FaChessPawn/>Play Online
+                        </button>
+                    </Link>
 
-                    <button className='home_section_computer home_buttons'>
-                        <FaRobot/>Play Computer
-                    </button>
-
+                    <Link to='/play-bot'>
+                        <button className='home_section_computer home_buttons'>
+                            <FaRobot/>Play Computer
+                        </button>
+                    </Link>
                 </section>
                 <section className='home_second_section'>
                     <p>Let´s play</p> 
